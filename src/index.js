bade52521da70655b8ac701886c3c3b42f5f6211
ruslan prakapets <nateriver345@gmail.com>
2021-08-16 11:16:47 +0300

    'Task_1'

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const element = <h1>Hello World меня зовут: Руслан Прокопец</h1>
+ReactDom.render(element, document.getElementById('root'))
